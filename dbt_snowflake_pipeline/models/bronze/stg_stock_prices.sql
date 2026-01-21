90e3d96b65cf5e3d1d3c9bf310256819add01172
Gianluca N Gozali <gianlucanicholasgozali@gmail.com>
2026-01-21 08:25:49 -0500

    test ci/cd

--- dbt_snowflake_pipeline/models/bronze/stg_stock_prices.sql
+++ dbt_snowflake_pipeline/models/bronze/stg_stock_prices.sql
@@ -40,7 +40,7 @@ formatted AS (
 
         -- Measures
         CAST(open_price AS FLOAT) AS open_price,
-        CAST(open_price AS FLOAT) * 100 AS open_price_times_100,
+        CAST(open_price AS FLOAT) * 200 AS open_price_times_200,
         CAST(high_price AS FLOAT) AS high_price,
         CAST(low_price AS FLOAT) AS low_price,
         CAST(close_price AS FLOAT) AS close_price,
